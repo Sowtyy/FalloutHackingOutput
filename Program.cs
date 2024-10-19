f5059464faaf7a424bb5bdec121003b1969be927
Sowtyy <38282990+Sowtyy@users.noreply.github.com>
2024-10-19 09:26:53 +0300

    added option to save output to file after completion; other changes that i kind of regret

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Text;
 
 namespace FalloutHackingOutput;
@@ -7,16 +9,24 @@ namespace FalloutHackingOutput;
 internal class Program
 {
     private const string _noiseCharacters = "!@#$%^&*()_+-=[]{}/\\,.<>?|~`:;'\"";
-    private List<string> _keywords = ["test"];
+    private readonly List<string> _keywords = ["test"];
 
     private uint? _maxRow = null;
     private uint? _maxRows = null;
     private uint? _keywordRate = null;
 
-    private List<string> GenerateNoiseRows()
-    {
-        List<string> rows = [];
+    private readonly List<string> _rows = [];
+    private string _rowsText = "";
 
+    private void SaveToFile()
+    {
+        string path = Process.GetCurrentProcess().ProcessName + "-output.txt";
+        File.WriteAllText(path, _rowsText);
+        Console.WriteLine($"\nSaved to '{path}'.");
+    }
+
+    private void GenerateNoiseRows()
+    {
         for (int i = 0; i < _maxRows; i++)
         {
             string row = "";
@@ -24,44 +34,40 @@ internal class Program
             for (int j = 0; j < _maxRow; j++)
                 row += _noiseCharacters[Random.Shared.Next(_noiseCharacters.Length)];
 
-            rows.Add(row);
+            _rows.Add(row);
         }
-
-        return rows;
     }
 
-    private List<string> AddWordsToRows(List<string> rows)
+    private void AddWordsToRows()
     {
-        List<string> newRows = [];
-
-        foreach (string row in rows)
+        for (int rowIndex = 0; rowIndex < _rows.Count; rowIndex++)
         {
+            string row = _rows[rowIndex];
             StringBuilder newRow = new StringBuilder(row);
 
             string word = _keywords[Random.Shared.Next(_keywords.Count)];
-            int rowIndex = Random.Shared.Next(newRow.Length);
+            int characterIndex = Random.Shared.Next(newRow.Length);
 
-            if (newRow.Length > rowIndex + word.Length &&
+            if (newRow.Length > characterIndex + word.Length &&
                 Random.Shared.Next((int)_keywordRate!) == 0)
             {
-                for (int i = rowIndex, j = 0; i < newRow.Length && j < word.Length; i++, j++)
+                for (int i = characterIndex, j = 0; i < newRow.Length && j < word.Length; i++, j++)
                     newRow[i] = word[j];
             }
 
-            newRows.Add(newRow.ToString());
+            _rows[rowIndex] = newRow.ToString();
         }
-
-        return newRows;
     }
 
-    private List<string> GenerateRows()
+    private void GenerateRows()
     {
-        return AddWordsToRows(GenerateNoiseRows());
+        GenerateNoiseRows();
+        AddWordsToRows();
     }
 
-    private static string RowsToText(List<string> rows)
+    private void RowsToText()
     {
-        return string.Join("\n", rows);
+        _rowsText = string.Join("\n", _rows);
     }
 
     private void AskSettings()
@@ -76,25 +82,43 @@ internal class Program
         if (toAskSettings)
             AskSettings();
 
-        Console.WriteLine("\n" + RowsToText(GenerateRows()));
+        _rows.Clear();
+        _rowsText = "";
+
+        GenerateRows();
+        RowsToText();
+
+        Console.WriteLine("\n" + _rowsText);
     }
 
     static void Main(string[] args)
     {
         Program program = new Program();
+
         bool toAskSettings = true;
+        bool toStart = true;
 
         while (true)
         {
-            program.Start(toAskSettings);
+            if (toStart)
+                program.Start(toAskSettings);
+            else
+                toStart = true;
 
             string input = Input.AskInput("\n" + """
                                           [Enter] - Try again
                                           [C]     - Try again with new settings
+                                          [S]     - Save to file
                                           [Q]     - Quit
                                           """ + " ").ToLower();
             if (input == "q")
                 break;
+            if (input == "s")
+            {
+                toStart = false;
+                program.SaveToFile();
+                continue;
+            }
             toAskSettings = input == "c";
         }
     }
